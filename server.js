8adf782129de81eb1c5c9c111e601879ac1fe3df
Edin93 <il_matador@hotmail.fr>
2017-12-22 12:39:40 +0100

    crud

---
+++ server.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const express = require('express')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+const errorHandler = require('errorhandler')
+const mongoDriver = require('mongodb').MongoClient
+
+const routes = require('./routes')
+const app = express()
+
+const DB_URL = 'mongodb://localhost:27017'
+const DATABASE = 'students-db'
+const PORT = process.env.PORT || 8080
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, 'dist')))
+
+mongoDriver.connect(DB_URL, function (err, client) {
+  if (err) throw err
+  let db = client.db(DATABASE)
+  let collection = db.collection('students')
+
+  app.use(function (req, res, next) {
+    req.collection = collection
+    next()
+  })
+
+  app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/dist/index.html')
+  })
+
+  app.get('/api/students', routes.students.getStudents)
+
+  app.get('/api/students/:id', routes.students.getStudent)
+
+  app.post('/api/students', routes.students.addStudent)
+
+  app.put('/api/students/:id', routes.students.updateStudent)
+
+  app.delete('/api/students/:id', routes.students.removeStudent)
+})
+
+app.use(errorHandler())
+
+app.listen(PORT, error => (
+  error
+    ? console.error(error)
+    : console.info(`Listening on port ${PORT}. Visit http://localhost:${PORT}/ in your browser.`)
+))
