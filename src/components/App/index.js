5b0e7124e2587162321beac39232afcc751dc600
Edin93 <il_matador@hotmail.fr>
2017-12-23 17:31:10 +0100

    all files are tracked again

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,32 +1,29 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 import StudentCard from '../StudentCard'
-
-import 'src/assets/stylesheets/base.scss';
+import 'src/assets/stylesheets/base.scss'
 
 import StudentsList from '../StudentsList'
 
 class App extends Component {
-	constructor(props){
-		super(props)
-		this.state = {
-			student: {
-				src: 'avatar.jpg' ,
-				fullname: 'Baligh Hamdi',
-				location: 'Tunis',
-				disponibility: 'Immediatly',
-				lookingFor: 'Internship',
-				skills: ['#HTML', '#CSS', '#JAVASCRIPT']
-			}
-		}
-	}
-  	render(){
+  constructor (props) {
+    super(props)
+    this.state = {
+      student: {
+        src: 'avatar.jpg',
+        fullname: 'Baligh Hamdi',
+        location: 'Tunis',
+        disponibility: 'Immediatly',
+        lookingFor: 'Internship',
+        skills: ['#HTML', '#CSS', '#JAVASCRIPT']
+      }
+    }
+  }
+  	render () {
   		return (
-    		<StudentCard student = {this.state.student} />
+    <StudentCard student={this.state.student} />
   		)
-	}
+  }
 };
 
-
-
-export default App;
+export default App
