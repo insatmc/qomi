5eeb90f056e0c7acd05e77f491b40b0795804816
Edin93 <il_matador@hotmail.fr>
2018-01-04 12:55:56 +0100

    workin on the student card

--- src/components/StudentCard/index.js
+++ src/components/StudentCard/index.js
@@ -7,62 +7,65 @@ class StudentCard extends Component {
     let student = this.props.student
     console.log('student in student card is ', student)
     return (
-      <div>
-        <div className='container'>
-          <div className='row'>
-            <div>
+      <div className='student-card'>
 
-              <div className='card hovercard'>
-                <div className='cardheader' />
-                <div className='avatar'>
-                  <img alt='student-image' src={student.image} />
-                </div>
-                <div className='info'>
-                  <div className='title'>
-                    <h2>{student.fullName}</h2>
-                  </div>
-                  <hr className='hr-style1' />
-                  <div className='desc'><h5>Location:</h5>{student.location}</div>
-                  <div className='desc'><h5>Disponibility:</h5>{student.disponibility}</div>
-                  <div className='desc'><h5>Looking For:</h5>
-                    {
-                    student.lookingFor.map((el, i) => <span className='lookingFor-container' key={i}>{el}</span>)
-                  }
-                  </div>
-                  <div className='desc'>
-                    <h5>Skills:</h5>
-                    <div>
-                      {
-                        student.skills.map((el, i) => <StudentSkill key={i} skill={el} />
-                        )
-                      }
-                    </div>
-                  </div>
-                  <hr className='hr-style2' />
-                </div>
-                <div className='bottom'>
-                  <a className='fa fa-facebook social-icon' rel='publisher' target='_blank' href='https://www.facebook.com/gomycode' />
+        <div className='student-image'>
+          <img alt='student-image' src={student.image} />
+        </div>
 
-                  <a className='btn btn-primary btn-twitter btn-sm' target='_blank' href={student.contacts.twitter}>
-                    <i className='fa fa-twitter' />
-                  </a>
-                  <a className='btn btn-danger btn-sm' rel='publisher'
-                    target='_blank' href={student.contacts.mail}>
-                    <i className='fa fa-google-plus' />
-                  </a>
-                  <a className='btn btn-secondary btn-sm ' rel='publisher'
-                    target='_blank' href={student.contacts.github}>
-                    <i className='fa fa-github' />
-                  </a>
-                  <a className='btn btn-primary btn-sm' rel='publisher' target='_blank' href={student.contacts.linkedin}>
-                    <i className='fa fa-linkedin' />
-                  </a>
-                </div>
-              </div>
+        <div className='student-name'>
+          {student.fullName}
+        </div>
 
-            </div>
+        <div className='student-location'>
+          <h6>Location:</h6>
+          <div className='student-info'>{student.location}</div>
+        </div>
+
+        <div className='student-disponibility'>
+          <h6>Disponibility:</h6>
+          <div className='student-info'>{student.disponibility}</div>
+        </div>
+
+        <div className='student-lookingFor'>
+          <h6>Looking for:</h6>
+          <div className='student-info'>
+            {
+            student.lookingFor.map((el, i) => <span className='lookingFor-container' key={i}>{el}</span>)
+          }
           </div>
         </div>
+
+        <div className='student-skills'>
+          <h6>Skills:</h6>
+          <div className='student-info'>
+            {
+            student.skills.map((el, i) => <StudentSkill key={i} skill={el} />)
+          }
+          </div>
+        </div>
+
+        <div className='student-social-icon'>
+          <a className='student-social-icon' rel='publisher' target='_blank' href={student.contacts.github}>
+            <i className='fa fa-github-square fa-2x' />
+          </a>
+          <a className='student-social-icon' rel='publisher' target='_blank' href={student.contacts.linkedin}>
+            <i className='fa fa-linkedin-square fa-2x' />
+          </a>
+          <a className='student-social-icon' rel='publisher' target='_blank' href={student.contacts.mail}>
+            <i className='fa fa-google-plus-square fa-2x' />
+          </a>
+          <a className='student-social-icon' rel='publisher' target='_blank' href={student.contacts.twitter}>
+            <i className='fa fa-twitter-square fa-2x' />
+          </a>
+        </div>
+
+        <div className='student-cv'>
+          <a target='_blank' href={student.cv}>
+            View CV
+          </a>
+        </div>
+
       </div>
     )
   }
