7949ae165357dd5db82d9cda87abc934d529d0cb
Edin93 <il_matador@hotmail.fr>
2018-01-10 19:09:57 +0100

    looking for filter search working

--- src/components/SubTabs/ContractSubTab.js
+++ src/components/SubTabs/ContractSubTab.js
@@ -1,15 +1,43 @@
 import React, {Component} from 'react'
 import './ContractSubTab.css'
+import axios from 'axios'
 
 class ContractSubTab extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      contracts: []
+    }
+  }
+
+  componentDidMount () {
+    axios.get('/api/students', {
+      validateStatus: function (status) {
+        return status < 400 // Reject only if the status code is greater than or equal to 500
+      }
+    }).then((data) => {
+      let students = data.data
+      let studentsLookingFor = students.map((el) => el.lookingFor)
+      let contracts = studentsLookingFor.reduce((acc, el) => acc = acc.concat(el), [])
+      this.setState({
+        contracts: contracts.filter((item, pos) => contracts.indexOf(item) == pos)
+      })
+    }).catch(function (error) {
+    })
+  }
+
   render () {
     return (
       <div className='input-container'>
-        <input onClick={this.props.onChangeContract} className='filter-input contract-input' type='button' value='Internship' /><br />
-        <input onClick={this.props.onChangeContract} className='filter-input contract-input' type='button' value='Freelance' /><br />
-        <input onClick={this.props.onChangeContract} className='filter-input contract-input' type='button' value='CDD' /><br />
-        <input onClick={this.props.onChangeContract} className='filter-input contract-input' type='button' value='CDI' /><br />
-        <input onClick={this.props.onChangeContract} className='filter-input contract-input' type='button' value='Others' /><br />
+        {
+          this.state.contracts.map((contract, i) => {
+            return (
+              <div key={i}>
+                <input onClick={this.props.onChangeContract} className='filter-input contract-input' type='button' value={contract} />
+              </div>
+            )
+          })
+        }
       </div>
     )
   }
