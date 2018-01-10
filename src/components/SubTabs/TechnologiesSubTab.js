f2cf87876c2b69ac49b88d6a773c63f7df96b22e
Edin93 <il_matador@hotmail.fr>
2018-01-10 19:32:12 +0100

    all 4 search filters code fixed

--- src/components/SubTabs/TechnologiesSubTab.js
+++ src/components/SubTabs/TechnologiesSubTab.js
@@ -1,35 +1,43 @@
 import React, {Component} from 'react'
 import './TechnologiesSubTab.css'
+import axios from 'axios'
 
 class TechnologiesSubTab extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      technologies: []
+    }
+  }
+
+  componentDidMount () {
+    axios.get('/api/students', {
+      validateStatus: function (status) {
+        return status < 400
+      }
+    }).then((data) => {
+      let students = data.data
+      let studentsTechnologies = students.map((el) => el.skills)
+      let technologies = studentsTechnologies.reduce((acc, el) => acc = acc.concat(el), [])
+      this.setState({
+        technologies: technologies.filter((item, pos) => technologies.indexOf(item) == pos)
+      })
+    }).catch(function (error) {
+    })
+  }
+
   render () {
     return (
       <div className='input-container'>
-        <input onClick={this.props.onChangeSkills} className='filter-input techno-input' type='button' value='HTML' /><br />
-        <input onClick={this.props.onChangeSkills} className='filter-input techno-input' type='button' value='CSS' /><br />
-        <input onClick={this.props.onChangeSkills} className='filter-input techno-input' type='button' value='Javascript' /><br />
-        <input onClick={this.props.onChangeSkills} className='filter-input techno-input' type='button' value='Bootstrap' /><br />
-        <input onClick={this.props.onChangeSkills} className='filter-input techno-input' type='button' value='Responsive Design' /><br />
-        <input onClick={this.props.onChangeSkills} className='filter-input techno-input' type='button' value='ReactJS' /><br />
-        <input onClick={this.props.onChangeSkills} className='filter-input techno-input' type='button' value='AngularJS' /><br />
-        <input onClick={this.props.onChangeSkills} className='filter-input techno-input' type='button' value='VueJS' /><br />
-        <input onClick={this.props.onChangeSkills} className='filter-input techno-input' type='button' value='React Native' /><br />
-        <input onClick={this.props.onChangeSkills} className='filter-input techno-input' type='button' value='NodeJS' /><br />
-        <input onClick={this.props.onChangeSkills} className='filter-input techno-input' type='button' value='ExpressJS' /><br />
-        <input onClick={this.props.onChangeSkills} className='filter-input techno-input' type='button' value='MongoDB' /><br />
-        <input onClick={this.props.onChangeSkills} className='filter-input techno-input' type='button' value='MeteorJS' /><br />
-        <input onClick={this.props.onChangeSkills} className='filter-input techno-input' type='button' value='Ruby on Rails' /><br />
-        <input onClick={this.props.onChangeSkills} className='filter-input techno-input' type='button' value='.NET' /><br />
-        <input onClick={this.props.onChangeSkills} className='filter-input techno-input' type='button' value='Java' /><br />
-        <input onClick={this.props.onChangeSkills} className='filter-input techno-input' type='button' value='Python' /><br />
-        <input onClick={this.props.onChangeSkills} className='filter-input techno-input' type='button' value='PHP' /><br />
-        <input onClick={this.props.onChangeSkills} className='filter-input techno-input' type='button' value='C#' /><br />
-        <input onClick={this.props.onChangeSkills} className='filter-input techno-input' type='button' value='Unity' /><br />
-        <input onClick={this.props.onChangeSkills} className='filter-input techno-input' type='button' value='Web Design' /><br />
-        <input onClick={this.props.onChangeSkills} className='filter-input techno-input' type='button' value='AI' /><br />
-        <input onClick={this.props.onChangeSkills} className='filter-input techno-input' type='button' value='Android Dev' /><br />
-        <input onClick={this.props.onChangeSkills} className='filter-input techno-input' type='button' value='Ios Dev' /><br />
-        <input onClick={this.props.onChangeSkills} className='filter-input techno-input' type='button' value='Others' /><br />
+        {
+          this.state.technologies.map((techno, i) => {
+            return (
+              <div key={i}>
+                <input onClick={this.props.onChangeSkills} className='filter-input techno-input' type='button' value={techno} />
+              </div>
+            )
+          })
+        }
       </div>
     )
   }
