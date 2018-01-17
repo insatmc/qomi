45888c21072db8ead477e99ba9cafba1ca0648b4
Edin93 <il_matador@hotmail.fr>
2018-01-17 21:52:03 +0100

    recruitments components added

--- src/components/RecruitsToDisplay/index.js
+++ src/components/RecruitsToDisplay/index.js
@@ -40,20 +40,20 @@ class RecruitsToDisplay extends Component {
           <h2 className='recruits-container-header'>Recruiters Applications</h2>
           <input className='form-control' onChange={this.searchRecruiter.bind(this)} placeholder='search offers by client name' />
         </header>
-        <div className='recruits-container'>
+        <div className='row recruits-container'>
           {
           this.state.recruits.filter((el) => el.clientFullname.toLowerCase().includes(this.state.recruiterToFind.toLowerCase())).map((el, i) => {
             return (
-              <div key={i} className='card recruiter-card'>
+              <div key={i} className='card recruiter-card col-xs-12 col-sm-5 col-md-5 col-lg-5'>
                 <div className='card-header'>
                   <h4>{el.clientFullname}</h4>
                 </div>
                 <div className='card-body'>
-                  <h5 className='card-title'><i className='fa fa-building' />            {el.companyName}</h5>
+                  <h5 className='card-title'><i className='fa fa-building' /> {el.companyName}</h5>
                   <ul className='list-group list-group-flush'>
-                    <li className='list-group-item'><i className='fa fa-phone' />            {el.clientPhone}</li>
-                    <li className='list-group-item'><i className='fa fa-envelope' />            {el.clientMail}</li>
-                    <li className='list-group-item'><i className='fa fa-link' />            {el.companyWebsite}</li>
+                    <li className='list-group-item'><i className='fa fa-phone' /> {el.clientPhone}</li>
+                    <li className='list-group-item'><i className='fa fa-envelope' /> {el.clientMail}</li>
+                    <li className='list-group-item'><i className='fa fa-link' /> {el.companyWebsite}</li>
                   </ul>
                   <div className='jumbotron jumbotron-fluid'>
                     <p>{el.clientMsg}</p>
